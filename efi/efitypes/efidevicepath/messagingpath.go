c4b5dffcd00621c1f4bd32be709bced3b02e178e
blnidson76 <blnidson76.gmail.com>
2024-07-08 18:39:32 +0300

    MessagingPath Subtypes

--- efi/efitypes/efidevicepath/messagingpath.go
+++ efi/efitypes/efidevicepath/messagingpath.go
@@ -15,11 +15,110 @@
 package efidevicepath
 
 import (
+	"fmt"
 	"io"
+	"net"
+
+	"github.com/blindson76/uefi/efi/efireader"
+)
+
+const (
+	MACAddressSubType = 11
+	IPv4DeviceSubType = 12
+	SATADeviceSubType = 18
 )
 
 func ParseMessagingDevicePath(r io.Reader, h Head) (p DevicePath, err error) {
-	p = &UnrecognizedDevicePath{Head: h}
+	switch h.SubType {
+	case MACAddressSubType:
+		p = &MACAddressDevicePath{Head: h}
+	case IPv4DeviceSubType:
+		p = &IPv4DevicePath{Head: h}
+	case SATADeviceSubType:
+		p = &SATADevicePath{Head: h}
+	default:
+		p = &UnrecognizedDevicePath{Head: h}
+	}
 	_, err = p.ReadFrom(r)
+	return
+}
+
+type MACAddressDevicePath struct {
+	Head
+	MAC      [32]byte
+	AddrType byte
+}
+
+func (p *MACAddressDevicePath) Text() string {
+	return fmt.Sprintf("MAC Address: %s Type: %d", net.HardwareAddr(p.MAC[:6]).String(), p.AddrType)
+}
+
+func (p *MACAddressDevicePath) GetHead() *Head {
+	return &p.Head
+}
+
+func (p *MACAddressDevicePath) ReadFrom(r io.Reader) (n int64, err error) {
+	fr := efireader.NewFieldReader(r, &n)
+
+	if err = fr.ReadFields(&p.MAC, &p.AddrType); err != nil {
+		return
+	}
+	return
+}
+
+type SATADevicePath struct {
+	Head
+	HBAPortNumber     uint16
+	PortMulPortNumber uint16
+	LUN               uint16
+}
+
+func (p *SATADevicePath) Text() string {
+	return fmt.Sprintf("SATA Port: %d PortMul: %d LUN: %d", p.HBAPortNumber, p.PortMulPortNumber, p.LUN)
+}
+
+func (p *SATADevicePath) GetHead() *Head {
+	return &p.Head
+}
+
+func (p *SATADevicePath) ReadFrom(r io.Reader) (n int64, err error) {
+	fr := efireader.NewFieldReader(r, &n)
+
+	if err = fr.ReadFields(&p.HBAPortNumber, &p.PortMulPortNumber, &p.LUN); err != nil {
+		return
+	}
+	return
+}
+
+type IPv4DevicePath struct {
+	Head
+	LocalIP     net.IP
+	RemoteIP    net.IP
+	LocalPort   uint16
+	RemotePort  uint16
+	Protocol    uint16
+	Static      bool
+	GatewayAddr net.IP
+	SubnetAddr  net.IP
+}
+
+func (p *IPv4DevicePath) Text() string {
+	return fmt.Sprintf("IPv4 Local:%s Remote:%s", p.LocalIP.String(), p.RemoteIP.String())
+}
+
+func (p *IPv4DevicePath) GetHead() *Head {
+	return &p.Head
+}
+
+func (p *IPv4DevicePath) ReadFrom(r io.Reader) (n int64, err error) {
+	fr := efireader.NewFieldReader(r, &n)
+	p.LocalIP = make(net.IP, 4)
+	p.RemoteIP = make(net.IP, 4)
+	p.SubnetAddr = make(net.IP, 4)
+	p.GatewayAddr = make(net.IP, 4)
+
+	if err = fr.ReadFields(&p.LocalIP, &p.RemoteIP, &p.LocalPort, &p.RemotePort); err != nil {
+		return
+	}
 	return
 }
